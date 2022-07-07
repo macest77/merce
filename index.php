d23db2795a4c05b509aa10248a1df8109e9c2874
macest77 <macest77@gmail.com>
2022-07-07 23:35:36 +0200

    index.php - handling method and data

--- index.php
+++ index.php
@@ -3,3 +3,25 @@ require __DIR__ . "/inc/bootstrap.php";
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
+
+$method = $_SERVER['REQUEST_METHOD'];
+$_request_data = array();
+$_code = 200;
+
+switch($method)
+{
+    case 'GET': 
+        $_request_data = $_GET;
+        break;
+    case 'POST':
+        $_request_data = $_POST;
+        break;
+    case 'PATCH':
+    case 'PUT':
+    case 'DELETE': 
+        parse_str(file_get_contents('php://input'), $_request_data);
+        break;
+
+    default:
+        $_code = 404;
+}
